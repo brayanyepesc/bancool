2499cf5b257583b4c44109d852c7999a22e0cc60
Brayan Yepes <abrayan.claye@gmail.com>
2024-08-23 12:00:17 -0500

    feat: fill account details sections with account details

--- src/app/components/molecules/AccountDetails/AccountDetails.tsx
+++ src/app/components/molecules/AccountDetails/AccountDetails.tsx
@@ -4,8 +4,11 @@ import { useStore } from "@/app/store/useStore";
 import { MoneyFormatter } from "@/app/utils/moneyFormatter";
 
 export const AccountDetails = () => {
-  const { currentUser, accounts } = useStore();
+  const { currentUser, accounts, transactions } = useStore();
   const userAccount = accounts.find((account) => account.userId === currentUser?.id);
+  const userTransactions = transactions.filter((transaction) => transaction.accountId === userAccount?.id);
+  const userExpenses = userTransactions.filter((transaction) => transaction.type === ('withdraw' || 'transfer'));
+  const balanceUserExpenses = userExpenses.reduce((acc, transaction) => acc + transaction.amount, 0);
   const balance = userAccount?.balance || 0;
   return (
     <div className="grid grid-cols-2 gap-4 w-full place-items-center">
@@ -15,11 +18,11 @@ export const AccountDetails = () => {
       </div>
       <div className="w-full h-44 bg-gray-100 rounded shadow-lg flex flex-col justify-center items-center space-y-2">
         <span className="text-sm font-bold text-gray-500">Expenses</span>
-        <p className="text-indigo-500 font-bold text-5xl">$200.000</p>
+        <p className="text-indigo-500 font-bold text-5xl">${MoneyFormatter(balanceUserExpenses)}</p>
       </div>
       <div className="w-full h-44 bg-gray-100 rounded shadow-lg flex flex-col justify-center items-center space-y-2">
         <span className="text-sm font-bold text-gray-500">Transactions</span>
-        <p className="text-indigo-500 font-bold text-5xl">2</p>
+        <p className="text-indigo-500 font-bold text-5xl">{userTransactions.length}</p>
       </div>
       <div className="w-full h-44 bg-gray-100 rounded shadow-lg flex flex-col justify-center items-center space-y-2">
         <span className="text-sm font-bold text-gray-500">Credits</span>
