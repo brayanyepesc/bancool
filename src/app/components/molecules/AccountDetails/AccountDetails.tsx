64014dbe9e8469c79f7a95d02934ea625b39b3a3
Brayan Yepes <abrayan.claye@gmail.com>
2024-08-23 11:09:54 -0500

    feat: add formatter money

--- src/app/components/molecules/AccountDetails/AccountDetails.tsx
+++ src/app/components/molecules/AccountDetails/AccountDetails.tsx
@@ -1,14 +1,16 @@
 'use client'
 
 import { useStore } from "@/app/store/useStore";
+import { MoneyFormatter } from "@/app/utils/moneyFormatter";
 
 export const AccountDetails = () => {
   const { currentUser } = useStore();
+  const balance = currentUser?.account?.balance || 0;
   return (
     <div className="grid grid-cols-2 gap-4 w-full place-items-center">
       <div className="w-full h-44 bg-gray-100 rounded shadow-lg flex flex-col justify-center items-center space-y-2">
         <span className="text-sm font-bold text-gray-500">Balance</span>
-        <p className="text-indigo-500 font-bold text-5xl">${currentUser?.account?.balance}</p>
+        <p className="text-indigo-500 font-bold text-5xl">${MoneyFormatter(balance)}</p>
       </div>
       <div className="w-full h-44 bg-gray-100 rounded shadow-lg flex flex-col justify-center items-center space-y-2">
         <span className="text-sm font-bold text-gray-500">Expenses</span>
